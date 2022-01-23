653966f0cd8c875b65d32d296769f61aa3964812
Jacekg1993 <jacekg93@gmail.com>
2022-01-23 07:41:41 +0100

    Setup Spring Boot project

---
+++ src/test/java/com/jacekg/teamfinder/TeamFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacekg.teamfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
