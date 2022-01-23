25c0fcf1106ef37e399a8b78a97594035a329afa
Jacekg1993 <jacekg93@gmail.com>
2022-01-23 12:06:26 +0100

    Created: PostgreSQL DB; User and Role tables

--- src/main/java/com/jacekg/teamfinder/TeamFinderApplication.java
+++ src/main/java/com/jacekg/teamfinder/TeamFinderApplication.java
@@ -2,8 +2,10 @@ package com.jacekg.teamfinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource({"classpath:application.properties", "classpath:secret.properties"})
 public class TeamFinderApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/jacekg/teamfinder/user/UserRepository.java
+++ src/main/java/com/jacekg/teamfinder/user/UserRepository.java
@@ -12,6 +12,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	@Query("FROM User u JOIN FETCH u.roles WHERE username=:username ")
 	User findByUsername(@Param("username") String username);
 	
-//	@Query("FROM User WHERE id=:userId")
+	@Query("FROM User WHERE id=:userId")
 	User findByUserId(@Param("userId") Long userId);
 }
