2d0e810b3ce6857a3e7ee066811dd7c45c91adb4
Jacekg93 <jacekg93@gmail.com>
2022-01-27 02:45:21 +0100

    Added: Exception handler

--- src/main/java/com/jacekg/teamfinder/config/WebSecurityConfig.java
+++ src/main/java/com/jacekg/teamfinder/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.jacekg.teamfinder.config;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
