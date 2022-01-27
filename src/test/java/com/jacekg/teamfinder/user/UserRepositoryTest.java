c4de6cc2e03b52c76bb9a8946858dcba3825f4a8
Jacekg93 <jacekg93@gmail.com>
2022-01-27 19:08:49 +0100

    Added: findByUsername, findByUserIdTests

--- src/test/java/com/jacekg/teamfinder/user/UserRepositoryTest.java
+++ src/test/java/com/jacekg/teamfinder/user/UserRepositoryTest.java
@@ -2,28 +2,69 @@ package com.jacekg.teamfinder.user;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
+
+import com.jacekg.teamfinder.role.Role;
 
 @DataJpaTest
 @DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
 class UserRepositoryTest {
 
-	@BeforeEach
-	void setUp() throws Exception {
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Test
+	void findByUsername_ShouldReturn_Valid_User() {
+		
+		String username = "user";
+		
+		User user = new User(
+				1L,
+				"user",
+				"password",	
+				"email@com",
+				true,
+				true,
+				true,
+				true, 
+				Arrays.asList(new Role(1L, "ROLE_USER"), new Role(2L, "ROLE_ADMIN")));
+		
+		userRepository.save(user);
+		
+		User returnedUser = userRepository.findByUsername(username);
+		
+		assertEquals("user", returnedUser.getUsername());
+		assertEquals("ROLE_ADMIN", returnedUser.getRoleName());
 	}
 
 	@Test
-	void testFindByUsername() {
-		fail("Not yet implemented");
+	void findByUserId_ShouldReturn_Valid_User() {
+		
+		Long userID = 1L;
+		
+		User user = new User(
+				1L,
+				"user",
+				"password",
+				"email@com",
+				true,
+				true,
+				true,
+				true, 
+				Arrays.asList(new Role(1L, "ROLE_USER"), new Role(2L, "ROLE_ADMIN")));
+		
+		userRepository.save(user);
+		
+		User returnedUser = userRepository.findByUserId(userID);
+		
+		assertEquals(1L, returnedUser.getId());
+		assertEquals("ROLE_ADMIN", returnedUser.getRoleName());
 	}
-
-	@Test
-	void testFindByUserId() {
-		fail("Not yet implemented");
-	}
-
 }
