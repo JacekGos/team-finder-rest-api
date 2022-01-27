7ca2cea0a3317cf793d0ed1acb77392366b31586
Jacekg93 <jacekg93@gmail.com>
2022-01-27 18:57:46 +0100

    Updated: UserRestController minor fixes

--- src/main/java/com/jacekg/teamfinder/user/UserRestController.java
+++ src/main/java/com/jacekg/teamfinder/user/UserRestController.java
@@ -4,11 +4,8 @@ import javax.validation.Valid;
 
 import static org.springframework.http.ResponseEntity.status;
 
-import java.security.Principal;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
