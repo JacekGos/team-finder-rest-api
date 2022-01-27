24904a611f9c6d97d545e8a30926b029633077ec
Jacekg93 <jacekg93@gmail.com>
2022-01-27 03:51:39 +0100

    Added: HttpMessageNotReadableException exeption handler

--- src/main/java/com/jacekg/teamfinder/exceptions/RestExceptionHandler.java
+++ src/main/java/com/jacekg/teamfinder/exceptions/RestExceptionHandler.java
@@ -2,12 +2,27 @@ package com.jacekg.teamfinder.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.jacekg.homefinances.config.ErrorResponse;
+import com.jacekg.homefinances.user.UserNotValidException;
 
 @ControllerAdvice
 public class RestExceptionHandler {
 	
+	@ExceptionHandler
+	public ResponseEntity<ErrorResponse> handleException(HttpMessageNotReadableException exc) {
+
+		ErrorResponse error = new ErrorResponse();
+
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		error.setMessage(exc.getMessage());
+		error.setErrorCode(1);
+		
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+	}
 }
 
 
