4d01968532b7e0f817189888dc1b483caa37de72
Jacekg93 <jacekg93@gmail.com>
2022-01-27 03:55:29 +0100

    Added: MethodArgumentNotValidException exception handler

--- src/main/java/com/jacekg/teamfinder/exceptions/RestExceptionHandler.java
+++ src/main/java/com/jacekg/teamfinder/exceptions/RestExceptionHandler.java
@@ -7,9 +7,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.jacekg.homefinances.config.ErrorResponse;
-import com.jacekg.homefinances.user.UserNotValidException;
-
 @ControllerAdvice
 public class RestExceptionHandler {
 	
