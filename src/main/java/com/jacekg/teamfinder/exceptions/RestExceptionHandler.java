99dc012427352c776793ee061b49cb39a57573ad
Jacekg93 <jacekg93@gmail.com>
2022-01-27 03:54:29 +0100

    Added: MethodArgumentNotValidException exception handler

--- src/main/java/com/jacekg/teamfinder/exceptions/RestExceptionHandler.java
+++ src/main/java/com/jacekg/teamfinder/exceptions/RestExceptionHandler.java
@@ -3,6 +3,7 @@ package com.jacekg.teamfinder.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -20,6 +21,18 @@ public class RestExceptionHandler {
 		error.setStatus(HttpStatus.BAD_REQUEST.value());
 		error.setMessage(exc.getMessage());
 		error.setErrorCode(1);
+		
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler
+	public ResponseEntity<ErrorResponse> handleException(MethodArgumentNotValidException exc) {
+
+		ErrorResponse error = new ErrorResponse();
+
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		error.setMessage(exc.getMessage());
+		error.setErrorCode(2);
 		
 		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
 	}
