19ce851987912b908323f903e12b754984444c86
Jacekg93 <jacekg93@gmail.com>
2022-02-02 11:24:58 +0100

    Added: GameRestController

---
+++ src/main/java/com/jacekg/teamfinder/game/GameRestController.java
@@ -0,0 +1,14 @@
+package com.jacekg.teamfinder.game;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/v1/game/")
+@AllArgsConstructor
+public class GameRestController {
+	
+	
+}
