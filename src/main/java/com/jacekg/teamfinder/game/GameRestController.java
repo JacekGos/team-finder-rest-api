293d0621700bc2e371c167545b865e94f780f700
Jacekg93 <jacekg93@gmail.com>
2022-02-03 09:16:42 +0100

    Added: in GameRestController - createGame method

--- src/main/java/com/jacekg/teamfinder/game/GameRestController.java
+++ src/main/java/com/jacekg/teamfinder/game/GameRestController.java
@@ -1,15 +1,29 @@
 package com.jacekg.teamfinder.game;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.ResponseEntity.status;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+
+import com.jacekg.teamfinder.user.UserResponse;
 
 import lombok.AllArgsConstructor;
 
 @RestController
-@RequestMapping("/v1/game/")
+@RequestMapping("/v1/")
 @AllArgsConstructor
 public class GameRestController {
 	
-	@PostMapping()
+	@PostMapping("/games")
+	public ResponseEntity<GameResponse> createGame(@Valid @RequestBody GameRequest gameRequest) {
+		
+		return status(HttpStatus.CREATED).body(gameService.save(gameRequest));
+	}
 }
