ff4c787ac9adaaaab9edc970ce19537305102ad3
Jacekg1993 <jacekg93@gmail.com>
2022-02-21 18:48:52 +0100

    Added: in GameServiceImpl test - save_ShouldReturn_Game test

--- src/test/java/com/jacekg/teamfinder/game/GameServiceImplTest.java
+++ src/test/java/com/jacekg/teamfinder/game/GameServiceImplTest.java
@@ -1,6 +1,13 @@
 package com.jacekg.teamfinder.game;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,23 +17,17 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 
-import com.jacekg.teamfinder.geocoding.model.GeocodeGeometry;
-import com.jacekg.teamfinder.geocoding.model.GeocodeLocation;
-import com.jacekg.teamfinder.geocoding.model.GeocodeObject;
 import com.jacekg.teamfinder.sport_discipline.SportDisciplineRepository;
 import com.jacekg.teamfinder.user.UserRepository;
 import com.jacekg.teamfinder.venue.TermRepository;
 import com.jacekg.teamfinder.venue.Venue;
 import com.jacekg.teamfinder.venue.VenueRepository;
-import com.jacekg.teamfinder.venue.VenueRequest;
-import com.jacekg.teamfinder.venue.VenueResponse;
-import com.jacekg.teamfinder.venue.VenueType;
 
 @ExtendWith(MockitoExtension.class)
 class GameServiceImplTest {
 	
 	@InjectMocks
-	GameServiceImpl gameServiceImpl;
+	GameServiceImpl serviceUnderTest;
 	
 	@Mock
 	private GameRepository gameRepository;
@@ -46,14 +47,45 @@ class GameServiceImplTest {
 	@Mock
 	private ModelMapper modelMapper;
 	
+	private GameRequest gameRequest;
+	
+	private Optional<Venue> venue;
+	
 	@BeforeEach
 	void setUp() {
 		
+		gameRequest = new GameRequest(
+				"gameName",
+				"sportDisciplineName",
+				10, 60, 25, 1,
+				LocalDate.now(),
+				10,
+				"game description"
+				);
+		
+		venue = Optional.ofNullable(new Venue(1L, "sport venue", "address 1", null, null, null));
 	}
 	
 	@Test
 	void save_ShouldReturn_Game() {
-		fail("Not yet implemented");
+		
+		when(venueRepository.findById(any(Long.class))).thenReturn(venue);
+		when(userRepository.findByUsername(anyString())).thenReturn(null);
+		when(sportDisciplineRepository.findByName(anyString())).thenReturn(null);
+
+		GameResponse game = serviceUnderTest.save(gameRequest, null);
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
