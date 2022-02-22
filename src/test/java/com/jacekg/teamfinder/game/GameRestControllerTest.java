9a215b7529a140f1f44dcaa6db9d76580a250b0d
Jacekg93 <jacekg93@gmail.com>
2022-02-22 10:13:30 +0100

    Added: in GameRestControllerTest - createGame_ShouldThrow_MethodArgumentNotValidException test

--- src/test/java/com/jacekg/teamfinder/game/GameRestControllerTest.java
+++ src/test/java/com/jacekg/teamfinder/game/GameRestControllerTest.java
@@ -25,6 +25,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jacekg.teamfinder.exceptions.ErrorResponse;
 import com.jacekg.teamfinder.jwt.JwtAuthenticationEntryPoint;
 import com.jacekg.teamfinder.jwt.JwtRequestFilter;
 import com.jacekg.teamfinder.user.User;
@@ -99,7 +100,6 @@ class GameRestControllerTest {
 				.andExpect(status().isCreated()).andReturn();
 		
 		String returnedGame = mvcResult.getResponse().getContentAsString();
-		System.out.println("returnedGame: " + mvcResult.getResponse().getContentLength());
 		
 		GameResponse gameResponse = objectMapper.readValue(returnedGame, GameResponse.class);
 		
@@ -112,6 +112,32 @@ class GameRestControllerTest {
 		assertThat(gameResponse).hasFieldOrPropertyWithValue("venueAddress", "address");
 		assertThat(gameResponse).hasFieldOrPropertyWithValue("date", LocalDateTime.of(LocalDate.of(2022, 1, 1), LocalTime.of(10, 0)));
 		assertThat(gameResponse).hasFieldOrPropertyWithValue("description", "game description");
+	}
+	
+	@Test
+	void createGame_ShouldThrow_MethodArgumentNotValidException() throws Exception {
+		
+		gameRequest.setName(null);
+		
+		TestingAuthenticationToken testingAuthenticationToken = new TestingAuthenticationToken(user,null);
+		
+		String jsonBody = objectMapper.writeValueAsString(gameRequest);
+		
+		when(gameService.save(any(GameRequest.class), any(Principal.class))).thenReturn(gameResponse);
+		
+		String url = "/v1/games";
+		
+		MvcResult mvcResult = mockMvc.perform(post(url)
+				.contentType(MediaType.APPLICATION_JSON)
+				.principal(testingAuthenticationToken)
+                .content(jsonBody))
+				.andExpect(status().isBadRequest()).andReturn();
+		
+		String responseContent = mvcResult.getResponse().getContentAsString();
+		
+		ErrorResponse errorResponse = objectMapper.readValue(responseContent, ErrorResponse.class);
+		
+		assertThat(errorResponse).hasFieldOrPropertyWithValue("message", "validation error");
 	}
 
 }
