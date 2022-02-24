0880e90da7cfad0141859db97cbdd76b8e9c2cb5
Jacekg93 <jacekg93@gmail.com>
2022-02-24 09:28:21 +0100

    Updated: in VenueRestController - findBySportDysciplineAndAddress passing parameters

--- src/main/java/com/jacekg/teamfinder/venue/VenueRestController.java
+++ src/main/java/com/jacekg/teamfinder/venue/VenueRestController.java
@@ -6,7 +6,10 @@ import static org.springframework.http.ResponseEntity.status;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
@@ -25,6 +29,8 @@ public class VenueRestController {
 	
 	private VenueService venueService;
 	
+	private static final Logger logger = LoggerFactory.getLogger(VenueRestController.class);
+	
 	@PostMapping("/venues")
 	public ResponseEntity<VenueResponse> createVenue
 		(@Valid @RequestBody VenueRequest venueRequest) throws IOException {
@@ -32,11 +38,20 @@ public class VenueRestController {
 		return status(HttpStatus.CREATED).body(venueService.save(venueRequest));
 	}
 	
-	@GetMapping("/venues/{sportDiscipline}/{address}")
-	public ResponseEntity<List<VenueResponse>> 
-		findBySportDysciplineAndAddress(@PathVariable String sportDiscipline, @PathVariable String address) throws IOException {
+//	@GetMapping("/venues/{sportDiscipline}/{address}")
+//	public ResponseEntity<List<VenueResponse>> 
+//		findBySportDysciplineAndAddress(@PathVariable String sportDiscipline, @PathVariable String address) throws IOException {
+//		
+//		return status(HttpStatus.OK)
+//				.body(venueService.findBySportDisciplineAndAddress(sportDiscipline, address));
+//	}
+	
+	@GetMapping("/venues")
+	public ResponseEntity<List<VenueResponse>> findBySportDysciplineAndAddress
+			(@RequestParam Map<String, String> venueQuery) throws IOException {
 		
-		return status(HttpStatus.OK).body(venueService.findBySportDisciplineAndAddress(sportDiscipline, address));
+		return status(HttpStatus.OK).body(venueService.findBySportDisciplineAndAddress
+				(venueQuery.get("sport_discipline"), venueQuery.get("address")));
 	}
 }
 
