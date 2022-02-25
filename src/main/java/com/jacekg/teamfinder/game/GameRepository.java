397ad680cf2cd4cd167d8a94bd2bae2f204cb617
Jacekg93 <jacekg93@gmail.com>
2022-02-25 09:59:31 +0100

    Updated: GameRepository - added JpaSpecificationExecutor and findAll method

--- src/main/java/com/jacekg/teamfinder/game/GameRepository.java
+++ src/main/java/com/jacekg/teamfinder/game/GameRepository.java
@@ -1,8 +1,12 @@
 package com.jacekg.teamfinder.game;
 
+import java.util.List;
+
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.QueryByExampleExecutor;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface GameRepository extends JpaRepository<Game, Long>, QueryByExampleExecutor<Game> {
-
+public interface GameRepository extends JpaRepository<Game, Long>, JpaSpecificationExecutor<Game> {
+	
+	public List<Game> findAll(Specification<Game> spec);
 }
