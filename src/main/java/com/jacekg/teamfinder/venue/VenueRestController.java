dd76605fb4a239149274019115c594a512a0d61c
Jacekg93 <jacekg93@gmail.com>
2022-03-01 19:39:09 +0100

    Updated VenueRestController: wrong import

--- src/main/java/com/jacekg/teamfinder/venue/VenueRestController.java
+++ src/main/java/com/jacekg/teamfinder/venue/VenueRestController.java
@@ -6,15 +6,12 @@ import static org.springframework.http.ResponseEntity.status;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +19,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
-import lombok.experimental.var;
 
 @RestController
 @RequestMapping("/v1")
